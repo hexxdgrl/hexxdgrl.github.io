cf7b7218fd5f751d575d7ee4a209c581c40ea6ce
hexxdgrl <hexxdgrl@github.com>
2024-08-18 19:17:29 -0600

    added dynamic navbar

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("sideNav").style.width = "250px";
+  document.getElementById("sideBarOpen").style.left = "-200px";
+}
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0px";
+  document.getElementById("sideBarOpen").style.left = "30px";
+}
